85bcbbbfaea8cf4b8a91e50e587caf6e3d886d74
seanmccray <94314592+seanmccray@users.noreply.github.com>
2022-03-02 10:01:09 -0500

    Add files via upload

---
+++ QuadcopterMain/flight_cointroller.ino
@@ -0,0 +1,68 @@
+#include <Wire.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+
+Adafruit_MPU6050 mpu;
+
+-----------------------------------------------------------------------------------------------------------------------
+//PID gain and limit settings
+
+float pid_p_gain_roll = 1.3;               //Gain setting for the roll P-controller
+float pid_i_gain_roll = 0.04;              //Gain setting for the roll I-controller
+float pid_d_gain_roll = 18.0;              //Gain setting for the roll D-controller
+int pid_max_roll = 400;                    //Maximum output of the PID-controller (+/-)
+
+float pid_p_gain_pitch = pid_p_gain_roll;  //Gain setting for the pitch P-controller.
+float pid_i_gain_pitch = pid_i_gain_roll;  //Gain setting for the pitch I-controller.
+float pid_d_gain_pitch = pid_d_gain_roll;  //Gain setting for the pitch D-controller.
+int pid_max_pitch = pid_max_roll;          //Maximum output of the PID-controller (+/-)
+
+float pid_p_gain_yaw = 4.0;                //Gain setting for the pitch P-controller. //4.0
+float pid_i_gain_yaw = 0.02;               //Gain setting for the pitch I-controller. //0.02
+float pid_d_gain_yaw = 0.0;                //Gain setting for the pitch D-controller.
+int pid_max_yaw = 400;                     //Maximum output of the PID-controller (+/-)
+
+boolean auto_level = true;                 //Auto level on (true) or off (false)
+-----------------------------------------------------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------------------------------------------------
+#define CHANNEL1 0
+#define CHANNEL2 1
+#define CHANNEL3 2
+#define CHANNEL4 3
+
+#define YAW      0
+#define PITCH    1
+#define ROLL     2
+#define THROTTLE 3
+
+#define X           0     // X axis
+#define Y           1     // Y axis
+#define Z           2     // Z axis
+#define MPU_ADDRESS 0x68  // I2C address of the MPU-6050
+#define FREQ        250   // Sampling frequency
+#define SSF_GYRO    65.5  // Sensitivity Scale Factor of the gyro from datasheet
+
+#define STOPPED  0
+#define STARTING 1
+#define STARTED  2
+-----------------------------------------------------------------------------------------------------------------------
+
+void setup() {
+Serial.begin(57600);
+
+//esc output pins set to outputs
+DDRD = B01000000;
+DDRA = B10000000;
+DDRC = B00101010;
+
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
