f1a3a621863ea843ffad088ff7d8068c120459f5
ColtonJE <ceisenreich99@hotmail.com>
2022-03-20 12:05:47 -0400

    started official controller code

--- ControllerCode/ControllerCode.ino
+++ ControllerCode/ControllerCode.ino
@@ -1,5 +1,35 @@
+/*
+ * Official controller code 
+ */
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+//Pin definitions ==========================
+#define CE_PIN 9
+#define CSN_PIN 10
+//==========================================
+
+//Controller Data Struct ===================
+typedef struct {
+  byte yaw;
+  byte pitch;
+  byte roll;
+  byte throttle;
+}conData;
+//==========================================
+
+//Radio Setup ==============================
+const byte thisSlaveAddress[5] = {'R','x','A','A','A'};
+
+RF24 radio(CE_PIN, CSN_PIN);
+
+conData dataRe
+//==========================================
+
 void setup() {
-  // put your setup code here, to run once:
+  
 
 }
 
