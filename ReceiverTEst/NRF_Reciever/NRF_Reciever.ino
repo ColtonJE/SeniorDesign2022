1535332d6b966466753cdffbb3a8acb72d0ce374
ColtonJE <ceisenreich99@hotmail.com>
2022-04-01 10:15:29 -0400

    Started adding servo control

--- ReceiverTEst/NRF_Reciever/NRF_Reciever.ino
+++ ReceiverTEst/NRF_Reciever/NRF_Reciever.ino
@@ -1,9 +1,12 @@
 // SimpleRx - the slave or the receiver
-
+//Libraries =====================================
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Servo.h>
+//=================================================
 
+//Radio ===========================================
 #define CE_PIN   9
 #define CSN_PIN 10
 
@@ -20,18 +23,44 @@ typedef struct {
 
 conData dataReceived; // this must match dataToSend in the TX
 bool newData = false;
+//===================================================
 
-//===========
+//Servo =============================================
+//Values for ESC pins
+byte escFL = 35;
+byte escFR = 33;
+byte escBL = 31;
+byte escBR = 29;
+// Initialize ESCs (Done as Servos to facilitate PWM signals)
+Servo ESC_FL;
+Servo ESC_FR;
+Servo ESC_BL;
+Servo ESC_BR;
+//===================================================
 
 void setup() {
+  // Radio =========================================
+  Serial.begin(9600);
 
-    Serial.begin(9600);
+  Serial.println("SimpleRx Starting");
+  radio.begin();
+  radio.setDataRate( RF24_250KBPS );
+  radio.openReadingPipe(1, thisSlaveAddress);
+  radio.startListening();
+  //================================================
 
-    Serial.println("SimpleRx Starting");
-    radio.begin();
-    radio.setDataRate( RF24_250KBPS );
-    radio.openReadingPipe(1, thisSlaveAddress);
-    radio.startListening();
+  //Servo Setup ====================================
+  // Attach Servos to corresponding Pins
+  ESC_FL.attach(escFL);
+  ESC_FR.attach(escFR);
+  ESC_BL.attach(escBL);
+  ESC_BR.attach(escBR);
+
+  ESC_FR.writeMicroseconds(1000);
+  ESC_FL.writeMicroseconds(1000);
+  ESC_BR.writeMicroseconds(1000);
+  ESC_BL.writeMicroseconds(1000);
+  //================================================
 }
 
 //=============
@@ -39,6 +68,12 @@ void setup() {
 void loop() {
     getData();
     showData();
+    if(newData){
+      ESC_FR.writeMicroseconds(map(dataReceived, 215, 255, 1000, 2000));
+      ESC_FL.writeMicroseconds(1000);
+      ESC_BR.writeMicroseconds(1000);
+      ESC_BL.writeMicroseconds(1000);
+    }
 }
 
 //==============
@@ -53,7 +88,15 @@ void getData() {
 void showData() {
     if (newData == true) {
         Serial.print("Data received ");
-        Serial.println(dataReceived.yaw);
+        Serial.print("\nYaw: " );
+        Serial.print((int)dataReceived.yaw);
+        Serial.print("\nPitch: ");
+        Serial.print((int)dataReceived.pitch);
+        Serial.print("\nRoll: ");
+        Serial.print((int)dataReceived.roll);
+        Serial.print("\nThrottle: ");
+        Serial.print((int)dataReceived.throttle);
+        Serial.print("\n");
         newData = false;
     }
 }
